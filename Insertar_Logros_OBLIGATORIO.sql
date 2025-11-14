8e982fa12dc5e13a739e506e3145682c15ae6660
Jhoans Aaron <onichanbb1@gmail.com>
2025-11-14 10:35:31 -0500

    style(Insertar_Logros): Cambio de extension txt a sql

--- Insertar_Logros_OBLIGATORIO.sql
+++ Insertar_Logros_OBLIGATORIO.sql
@@ -1,4 +1,4 @@
-La sentencia SQL a continuación permite insertar logros, si no se realiza la inserción, la plataforma no ejecutará.
+--La sentencia SQL a continuación permite insertar logros, si no se realiza la inserción, la plataforma no ejecutará.
 
 INSERT INTO logro (nombre, descripcion, imagen_url) VALUES
 ('Miembro de la comunidad', 'Se unió a AddVenture', 'logroConfiable.png'),
