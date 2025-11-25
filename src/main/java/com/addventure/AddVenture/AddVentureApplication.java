532e1e48f84a2c656d2cffd1129c1ab565d912f8
Jhoans Aaron <onichanbb1@gmail.com>
2025-11-25 11:53:30 -0500

    refactor: integrar lectura de variables de entorno para mas seguridad

--- src/main/java/com/addventure/AddVenture/AddVentureApplication.java
+++ src/main/java/com/addventure/AddVenture/AddVentureApplication.java
@@ -4,11 +4,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableScheduling
 @SpringBootApplication
 public class AddVentureApplication {
 
 	public static void main(String[] args) {
+
+		// Cargar solo la contraseña desde el archivo .env
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env")
+				.directory("src/main/resources")
+				.load();
+
+		System.setProperty("BD_PASSWORD", dotenv.get("BD_PASSWORD"));
+
 		SpringApplication.run(AddVentureApplication.class, args);
 	}
 
